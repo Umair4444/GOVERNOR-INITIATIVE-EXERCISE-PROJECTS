4f03ac001af58c434274bdcb11e3a1596559b8ce
Umair Ali Khan <100639296+Umair4444@users.noreply.github.com>
2024-12-09 00:39:34 +0500

    update

--- CLONE/hackathon-coffee-tea-shop/src/app/menu/page.tsx
+++ CLONE/hackathon-coffee-tea-shop/src/app/menu/page.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import TopCard from "../components/TopCard";
 import MenuCard from "../components/MenuCard";
+import Menucard3 from "@/app/assets/menu3.png";
+import Menucard4 from "@/app/assets/menu4.png";
 
-const Page = () => {
+const MenuPage = () => {
   return (
     <div>
       <div>
         <TopCard />
       </div>
       <div>
-        <MenuCard />
+        <MenuCard image={Menucard3} />
       </div>
     </div>
   );
 };
+
+export default MenuPage;
