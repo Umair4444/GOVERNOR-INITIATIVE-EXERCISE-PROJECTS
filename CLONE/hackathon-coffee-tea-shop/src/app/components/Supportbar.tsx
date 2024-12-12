817afb47cba8f6819fcd46e2ad8b7f771e974e9d
Umair Ali Khan <100639296+Umair4444@users.noreply.github.com>
2024-12-12 19:57:04 +0500

    update

--- CLONE/hackathon-coffee-tea-shop/src/app/components/Supportbar.tsx
+++ CLONE/hackathon-coffee-tea-shop/src/app/components/Supportbar.tsx
@@ -5,9 +5,8 @@ const Supportbar = () => {
     <div className="flex flex-col">
       <div className="flex items-center justify-around sm:px-14 md:p-0">
         <div className="flex flex-col lg:gap-3 sm:gap-2 items-start px-4">
-          <h2 className="font-bold lg:text-3xl md:text-2xl sm:text-xl">
-            <span className="text-yellow-500">St</span>ill You Need Our Support
-            ?
+          <h2 className="font-bold lg:text-3xl md:text-2xl sm:text-lg">
+            <span className="text-yellow-500">St</span>ill You Need Our Support?
           </h2>
           <p className="lg:text-lg text-wrap sm:hidden md:block ">
             Don’t wait make a smart & logical quote here. Its pretty easy.
